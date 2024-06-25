db633426230d0b380bde5353ccfe727abe9c8b3a
Md Akash <you@example.com>
2024-06-26 00:21:34 +0600

    Frontend Edit

--- admin/include/sidebar.php
+++ admin/include/sidebar.php
@@ -6,7 +6,7 @@
                     <ul class="metismenu" id="menu1">
                         <li class="active">
                             <a class="has-arrow" href="index.html">
-								   <span class="educate-icon educate-home icon-wrap"></span>
+								   <span style="color:white" class="educate-icon educate-home icon-wrap"></span>
 								   <span class="mini-click-non">Menu</span>
 								</a>
                             <ul class="submenu-angle" aria-expanded="true">
@@ -15,20 +15,20 @@
                             </ul>
                         </li>
                     <li>
-                        <a class="has-arrow" href="javascript:void(0)" aria-expanded="false"><span class="educate-icon educate-event icon-wrap sub-icon-mg" aria-hidden="true"></span> <span class="mini-click-non">User</span></a>
+                        <a class="has-arrow" href="javascript:void(0)" aria-expanded="false"><span style="color:white" class="educate-icon educate-event icon-wrap sub-icon-mg" aria-hidden="true"></span> <span class="mini-click-non">User</span></a>
                         <ul class="nav nav-second-level" aria-expanded="false">
                             <li><a title="All User" href="<?= $baseurl ?>authentication_list.php"><span class="mini-sub-pro">List</span></a></li>
                         </ul>
                     </li>
                     <li>
-                        <a class="has-arrow" href="javascript:void(0)" aria-expanded="false"><span class="educate-icon educate-professor icon-wrap"></span> <span class="mini-click-non">Customer</span></a>
+                        <a class="has-arrow" href="javascript:void(0)" aria-expanded="false"><span style="color:white" class="educate-icon educate-professor icon-wrap"></span> <span class="mini-click-non">Customer</span></a>
                         <ul class="nav nav-second-level" aria-expanded="false">
                             <li><a title="All Customer" href="<?= $baseurl ?>customer_add.php"><span class="mini-sub-pro">Add New</span></a></li>
                             <li><a title="Add Customer" href="<?= $baseurl ?>customer_list.php"><span class="mini-sub-pro">List</span></a></li>
                         </ul>
                     </li>
                     <li>
-                        <a class="has-arrow" href="javascript:void(0)" aria-expanded="false"><span class="educate-icon educate-student icon-wrap"></span> <span class="mini-click-non">Supplier</span></a>
+                        <a class="has-arrow" href="javascript:void(0)" aria-expanded="false"><span style="color:white" class="educate-icon educate-student icon-wrap"></span> <span class="mini-click-non">Supplier</span></a>
                         <ul class="nav nav-second-level" aria-expanded="false">
                             <li><a title="All Supplier" href="<?= $baseurl ?>supplier_add.php"><span class="mini-sub-pro">Add New</span></a></li>
                             <li><a title="Add Supplier" href="<?= $baseurl ?>supplier_list.php"><span class="mini-sub-pro">List</span></a></li>
@@ -36,35 +36,35 @@
                         </ul>
                     </li>
                     <li>
-                        <a class="has-arrow" href="javascript:void(0)" aria-expanded="false"><span class="educate-icon educate-course icon-wrap"></span> <span class="mini-click-non">Category</span></a>
+                        <a class="has-arrow" href="javascript:void(0)" aria-expanded="false"><span style="color:white" class="educate-icon educate-course icon-wrap"></span> <span class="mini-click-non">Category</span></a>
                         <ul class="nav nav-second-level" aria-expanded="false">
                             <li><a title="All Items" href="<?= $baseurl ?>category_add.php"><span class="mini-sub-pro">Add New</span></a></li>
                             <li><a title="Add Items" href="<?= $baseurl ?>category_list.php"><span class="mini-sub-pro">List</span></a></li>
                         </ul>
                     </li>
                     <li>
-                        <a class="has-arrow" href="javascript:void(0)" aria-expanded="false"><span class="educate-icon educate-course icon-wrap"></span> <span class="mini-click-non">Items</span></a>
+                        <a class="has-arrow" href="javascript:void(0)" aria-expanded="false"><span style="color:white" class="educate-icon educate-course icon-wrap"></span> <span class="mini-click-non">Items</span></a>
                         <ul class="nav nav-second-level" aria-expanded="false">
                             <li><a title="All Items" href="<?= $baseurl ?>items_add.php"><span class="mini-sub-pro">Add New</span></a></li>
                             <li><a title="Add Items" href="<?= $baseurl ?>items_list.php"><span class="mini-sub-pro">List</span></a></li>
                         </ul>
                     </li>
                     <li>
-                        <a class="has-arrow" href="all-courses.html" aria-expanded="false"><span class="educate-icon educate-library icon-wrap"></span> <span class="mini-click-non">Purchase</span></a>
+                        <a class="has-arrow" href="all-courses.html" aria-expanded="false"><span style="color:white" class="educate-icon educate-library icon-wrap"></span> <span class="mini-click-non">Purchase</span></a>
                         <ul class="nav nav-second-level" aria-expanded="false">
                         <li><a title="purchase_add" href="<?= $baseurl ?>purchase_add.php"><span class="mini-sub-pro">Add New</span></a></li>
                         <li><a title="purchase_list" href="<?= $baseurl ?>purchase_list.php"><span class="mini-sub-pro">List</span></a></li>
                         </ul>
                     </li>
                     <li>
-                        <a class="has-arrow" href="javascript:void(0)" aria-expanded="false"><span class="educate-icon educate-library icon-wrap" ></span> <span class="mini-click-non">Sales</span></a>
+                        <a class="has-arrow" href="javascript:void(0)" aria-expanded="false"><span style="color:white" class="educate-icon educate-library icon-wrap" ></span> <span class="mini-click-non">Sales</span></a>
                         <ul class="nav nav-second-level" aria-expanded="false">
                             <li><a title="Add items" href="<?= $baseurl ?>sales_add.php"><span class="mini-sub-pro">Add New</span></a></li>
                             <li><a title="List Items" href="<?= $baseurl ?>sales_list.php"><span class="mini-sub-pro">List</span></a></li>
                         </ul>
                     </li>
                     <li>
-                        <a class="has-arrow" href="all-courses.html" aria-expanded="false"><span class="educate-icon educate-course icon-wrap"></span> <span class="mini-click-non">Stock</span></a>
+                        <a class="has-arrow" href="all-courses.html" aria-expanded="false"><span style="color:white" class="educate-icon educate-course icon-wrap"></span> <span class="mini-click-non">Stock</span></a>
                         <ul class="nav nav-second-level" aria-expanded="false">
                         <li><a title="Stock_add" href="<?= $baseurl ?>stock_add.php"><span class="mini-sub-pro">Add New</span></a></li>
                         <li><a title="Stock_list" href="<?= $baseurl ?>stock_list.php"><span class="mini-sub-pro">List</span></a></li>
--- admin/index.php
+++ admin/index.php
@@ -206,10 +206,10 @@
                     <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
                         <div class="single-cards-item">
                             <div class="single-product-image">
-                                <a href="#"><img src="img/product/profile-bg.jpg" alt=""></a>
+                                <a href="#"><img src="assets/assets/img/product/profile-bg.jpg" alt=""></a>
                             </div>
                             <div class="single-product-text">
-                                <img src="img/product/pro4.jpg" alt="">
+                                <img src="assets/img/product/pro4.jpg" alt="">
                                 <h4><a class="cards-hd-dn" href="#">Angela Dominic</a></h4>
                                 <h5>Web Designer & Developer</h5>
                                 <p class="ctn-cards">Lorem ipsum dolor sit amet, this is a consectetur adipisicing elit</p>
@@ -243,7 +243,7 @@
                                 <h2>Reviews</h2>
                             </div>
                             <div class="single-review-st-text">
-                                <img src="img/notification/1.jpg" alt="">
+                                <img src="assets/img/notification/1.jpg" alt="">
                                 <div class="review-ctn-hf">
                                     <h3>Sarah Graves</h3>
                                     <p>Highly recommend</p>
@@ -257,7 +257,7 @@
                                 </div>
                             </div>
                             <div class="single-review-st-text">
-                                <img src="img/notification/2.jpg" alt="">
+                                <img src="assets/img/notification/2.jpg" alt="">
                                 <div class="review-ctn-hf">
                                     <h3>Garbease sha</h3>
                                     <p>Awesome Pro</p>
@@ -271,7 +271,7 @@
                                 </div>
                             </div>
                             <div class="single-review-st-text">
-                                <img src="img/notification/3.jpg" alt="">
+                                <img src="assets/img/notification/3.jpg" alt="">
                                 <div class="review-ctn-hf">
                                     <h3>Gobetro pro</h3>
                                     <p>Great Website</p>
@@ -285,7 +285,7 @@
                                 </div>
                             </div>
                             <div class="single-review-st-text">
-                                <img src="img/notification/4.jpg" alt="">
+                                <img src="assets/img/notification/4.jpg" alt="">
                                 <div class="review-ctn-hf">
                                     <h3>Siam Graves</h3>
                                     <p>That's Good</p>
@@ -299,7 +299,7 @@
                                 </div>
                             </div>
                             <div class="single-review-st-text">
-                                <img src="img/notification/5.jpg" alt="">
+                                <img src="assets/img/notification/5.jpg" alt="">
                                 <div class="review-ctn-hf">
                                     <h3>Sarah Graves</h3>
                                     <p>Highly recommend</p>
@@ -313,7 +313,7 @@
                                 </div>
                             </div>
                             <div class="single-review-st-text">
-                                <img src="img/notification/6.jpg" alt="">
+                                <img src="assets/img/notification/6.jpg" alt="">
                                 <div class="review-ctn-hf">
                                     <h3>Julsha Grav</h3>
                                     <p>Sei Hoise bro</p>
@@ -331,7 +331,7 @@
                     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                         <div class="single-product-item res-mg-t-30 table-mg-t-pro-n tb-sm-res-d-n dk-res-t-d-n">
                             <div class="single-product-image">
-                                <a href="#"><img src="img/product/book-4.jpg" alt=""></a>
+                                <a href="#"><img src="assets/img/product/book-4.jpg" alt=""></a>
                             </div>
                             <div class="single-product-text edu-pro-tx">
                                 <h4><a href="#">Title Demo Here</a></h4>
@@ -510,7 +510,7 @@
                     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                         <div class="courses-inner res-mg-t-30 table-mg-t-pro-n tb-sm-res-d-n dk-res-t-d-n">
                             <div class="courses-title">
-                                <a href="#"><img src="img/courses/1.jpg" alt="" /></a>
+                                <a href="#"><img src="assets/img/courses/1.jpg" alt="" /></a>
                                 <h2>Apps Development</h2>
                             </div>
                             <div class="courses-alaltic">
