5364d1648bf8de74c5deb3b8eeea444281fbccdd
abufoysal581 <abufoysal581@gmail.com>
2024-07-14 10:58:49 +0600

    stock table update

--- admin/project doc/stock.sql
+++ admin/project doc/stock.sql
@@ -1,31 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 14, 2024 at 06:56 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `sports_store`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `stock`
---
 
 CREATE TABLE `stock` (
   `id` int(11) NOT NULL,
@@ -42,36 +19,10 @@ CREATE TABLE `stock` (
   `deleted_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `stock`
---
 
-INSERT INTO `stock` (`id`, `purchase_id`, `sales_id`, `item_id`, `qty`, `price`, `stock_date`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`) VALUES
-(1, 4, NULL, 5, '55.00', '230.00', '2024-07-14', '2024-07-14 05:38:59', 24, NULL, NULL, NULL),
-(2, NULL, 24, 7, '1.00', '100.00', '2024-07-14', '2024-07-14 06:05:25', 1, NULL, NULL, NULL),
-(3, NULL, 25, 7, '-3.00', '100.00', '2024-07-14', '2024-07-14 06:05:57', 1, NULL, NULL, NULL);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `stock`
---
 ALTER TABLE `stock`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `stock`
---
 ALTER TABLE `stock`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
